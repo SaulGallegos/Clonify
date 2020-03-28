cf01a88a0898989f688398e04ddfbafd10b3bbf4
SaulGallegos <45114955+SaulGallegos@users.noreply.github.com>
2020-03-27 18:21:20 -0600

    agregarerror

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,11 +7,16 @@ import { SpotifyService } from '../../services/spotify.service';
 })
 export class HomeComponent {
   nuevasCanciones: any[] = [];
+  error = false;
+  mensaje: string;
   constructor(private spotify: SpotifyService) {
     this.spotify.getNewReleases()
       .subscribe(data => {
         console.log(data);
         this.nuevasCanciones = data;
+      }, (er) => {
+        this.error = true;
+        this.mensaje = er.error.error.message;
       });
   }
 
